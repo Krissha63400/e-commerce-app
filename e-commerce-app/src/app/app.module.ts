5f6d4efa8e5de8cddb148aa78adcdf37a2e138b7
Batol <krissha.joy.batol@accenture.com>
2023-06-20 11:08:36 +0800

    start again

--- e-commerce-app/src/app/app.module.ts
+++ e-commerce-app/src/app/app.module.ts
@@ -2,29 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { HeaderComponent } from './header/header.component';
-<<<<<<< HEAD
-=======
+
 import { HomeComponent } from './home/home.component';
->>>>>>> member
-=======
 import { ProductComponent } from './product/product.component';
->>>>>>> f69d3bc979e46c64c1b677716445885d578bc09c
+
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-<<<<<<< HEAD
-    HeaderComponent
-=======
-    HomeComponent
->>>>>>> member
-=======
+    HeaderComponent,
+    HomeComponent,
     HeaderComponent,
     ProductComponent
->>>>>>> f69d3bc979e46c64c1b677716445885d578bc09c
   ],
   imports: [
     BrowserModule
