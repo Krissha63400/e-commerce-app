92dba78f93218c8dabc5c3e4ddd26421ca041e56
aiybarrera <aiyreinestybarrera.adc@gmail.com>
2023-06-26 16:31:48 +0800

    pull3

--- e-commerce-app/src/app/app.module.ts
+++ e-commerce-app/src/app/app.module.ts
@@ -2,11 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http'
-
+import { ProductComponent } from './product/product.component'
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
-import { ProductComponent } from './product/product.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import { SignInOutComponent } from './sign-in-out/sign-in-out.component';
--- e-commerce-app/src/app/product/product.component.ts
+++ e-commerce-app/src/app/product/product.component.ts
@@ -1,54 +1,15 @@
-<<<<<<< HEAD
-import { Component, OnInit } from '@angular/core';
-import { ProductService } from 'src/app/services/product.service';
-=======
 import { Component } from '@angular/core';
 import { ProductsDataService } from '../products-data.service';
 
->>>>>>> 8fee26b96366366d5768d50805c84d32e7319d9b
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
 })
-export class HomeComponent implements OnInit{
+export class ProductComponent{
 
-<<<<<<< HEAD
-  productList: any [] = [];
-  cartObj : any = {
-    "CartId": 0,
-    "CustId": 1,
-    "ProductId": 0,
-    "Quantity": 0,
-    "AddedDate": "2023-04-27T07:12:40.926Z"
-  };
-  constructor(private productService: ProductService) {
 
-  }
-  ngOnInit(): void {
-    debugger;
-    this.loadAllProducts();
-  }
-
-  loadAllProducts() {
-    debugger;
-    this.productService.getAllProducts().subscribe((result: any)=>{
-      this.productList = result.data;
-    })
-  }
-
-  addItemToCart(productId: number) {
-    debugger;
-    this.cartObj.ProductId = productId;
-    this.productService.addToCart(this.cartObj).subscribe((result: any)=>{
-       if(result.result) {
-        alert("Product Added To Cart");
-        this.productService.cartAddedSubject.next(true);
-       }
-    })
-  }
-=======
   constructor(public productsDataService: ProductsDataService) {}
->>>>>>> 8fee26b96366366d5768d50805c84d32e7319d9b
+
 }
